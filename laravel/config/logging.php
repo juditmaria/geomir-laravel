3c17ffcfd03024895042e5a8c82696776bfe9ebf
judit maria <jumoar@fp.insjoaquimmir.cat>
2022-10-25 19:47:50 +0200

    [M7] Plus Servidor de Correu

--- laravel/config/logging.php
+++ laravel/config/logging.php
@@ -1,5 +1,4 @@
 <?php
-
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
@@ -53,19 +52,19 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'errorlog'],
             'ignore_exceptions' => false,
         ],
 
-        'single' => [
+        'file' => [
             'driver' => 'single',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
+        'errorlog' => [
+            'driver' => 'errorlog',
+            'path' => storage_path('LOG_LEVEL', 'debug'),
             'level' => env('LOG_LEVEL', 'debug'),
             'days' => 14,
         ],
@@ -118,5 +117,24 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
     ],
+
+'channels' => [
+       'stack' => [
+           'driver' => 'stack',
+           'channels' => ['file', 'errorlog'],
+           'ignore_exceptions' => false,
+       ],
+      
+       'file' => [
+           'driver' => 'single',
+           'path' => storage_path('logs/laravel.log'),
+           'level' => env('LOG_LEVEL', 'debug'),
+       ],
+ 
+       'errorlog' => [
+           'driver' => 'errorlog',
+           'level' => env('LOG_LEVEL', 'debug'),
+       ]
+   ],
 
 ];
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use Illuminate\Support\Facades\Log;
+ 
+Route::get('/', function () {
+   Log::info('Loading welcome page');
+   return view('welcome');
+});
+
+use App\Http\Controllers\MailController;
+// ...
+Route::get('mail/test', [MailController::class, 'test']);
+// or
+// Route::get('mail/test', 'App\Http\Controllers\MailController@test');
