d84eef26af8d4ad414b647288405bf1de743371d
Max <113206606+Maxmmir@users.noreply.github.com>
2022-11-07 12:57:38 +0100

    Update dashboard.blade.php

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -1,37 +1,29 @@
-<!-- @extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{_('Dashboard') }}
+        </h2>
+    </x slot>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-                
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class ="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    You're logged in!
                 </div>
-                @extends('layouts.app')
- 
-
-
-                </div>
-            </div>
+            <div>
         </div>
     </div>
-</div>
-@endsection -->
-<x-app-layout>
-    x-slot name="header"
+    @section('content')
+    <h2>{{__('Resources') }}</h2>
+    <a href="{{ url('/files') }}">{{__('Files') }}</a>
+
+    <a href="{{ url('/places') }}">{{__('Places') }}</a>
+    @endsection
     
 
 
 </x-app-layout>
-
 
 
